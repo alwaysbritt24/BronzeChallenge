d70df97aad000131421081a50c562ae6d3b73c37
alwaysbritt24 <bslee05@icloud.com>
2025-09-19 10:21:58 -0400

    added background gradient

--- World/ConversionViewController.swift
+++ World/ConversionViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ConversionViewController: UIViewController {
+    
+    private let gradientLayer = CAGradientLayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         print("ConversionViewController loaded its view.")
+        
+        view.backgroundColor = .clear
+        
+        gradientLayer.colors = [
+            UIColor.systemPurple.cgColor, // Top
+            UIColor.systemBlue.cgColor,
+            UIColor.systemGreen.cgColor,
+            UIColor.systemYellow.cgColor,
+            UIColor.systemRed.cgColor ]
+               
+        gradientLayer.locations = [0.0, 0.25, 0.5, 0.75, 1.0]
+        
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
+        
+        gradientLayer.frame = view.bounds
+        
+        view.layer.insertSublayer(gradientLayer, at: 0)
     }
+        override func viewWillLayoutSubviews() {
+               super.viewWillLayoutSubviews()
+               gradientLayer.frame = view.bounds
+           }
+        
+    
 
 
 
